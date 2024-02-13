e403896d972674de03b2b84fa16c2bc9c4d56794
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-02-13 22:12:56 +0300

    add local configuration

--- init.lua
+++ init.lua
@@ -3,9 +3,21 @@ local vimrc = config .. '/../../.vimrc'
 if vim.fn.filereadable(vimrc) ~= 0 then
 	vim.cmd.source(vimrc)
 end
+
 require 'setup.plugins'
 require 'setup.style'
 require 'setup.remap'
 require 'setup.terminal'
-vim.o.cmdheight = 0
 
+local local_file = config .. '/lua/local.lua'
+if vim.fn.filereadable(local_file) ~= 0 then
+	vim.cmd.source(local_file)
+end
+
+local local_setup = config .. '/lua/local'
+if vim.fn.isdirectory(local_setup) ~= 0 then
+	local files = vim.fn.glob(local_setup .. '/*.lua')
+	for file in string.gmatch(files, "([^\n]+)") do
+		vim.cmd.source(file)
+	end
+end
