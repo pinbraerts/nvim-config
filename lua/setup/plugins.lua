a7b4129a8a0cc576d64f7869d7046e73a8f4ad47
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-02-21 20:38:25 +0300

    completion: add signature help

--- lua/setup/plugins.lua
+++ lua/setup/plugins.lua
@@ -291,6 +291,7 @@ require 'lazy'.setup {
 		'hrsh7th/nvim-cmp',
 		dependencies = {
 			'hrsh7th/cmp-nvim-lsp',
+			'hrsh7th/cmp-nvim-lsp-signature-help',
 			'hrsh7th/cmp-buffer',
 			'hrsh7th/cmp-path',
 			'hrsh7th/cmp-cmdline',
