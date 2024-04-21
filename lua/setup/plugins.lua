eb54a4ee85343948598ad8fd653021cc9ec957ae
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-04-21 17:39:08 +0300

    telescope: use on vim.ui.select

--- lua/setup/plugins.lua
+++ lua/setup/plugins.lua
@@ -451,6 +451,14 @@ require 'lazy'.setup {
 	},
 
 	{
+		'nvim-telescope/telescope-ui-select.nvim',
+		dependencies = { 'nvim-telescope/telescope.nvim' },
+		config = function()
+			require('telescope').load_extension('ui-select')
+		end,
+	},
+
+	{
 		'nvim-telescope/telescope-file-browser.nvim',
 		dependencies = { 'nvim-telescope/telescope.nvim' },
 		config = function()
