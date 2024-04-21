890211165f65713ff3601cd5d49498e14356f917
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-04-21 16:05:58 +0300

    luasnip: install jsregexp

--- lua/setup/plugins.lua
+++ lua/setup/plugins.lua
@@ -493,7 +493,10 @@ require 'lazy'.setup {
 			'hrsh7th/cmp-path',
 			'hrsh7th/cmp-cmdline',
 			'petertriho/cmp-git',
-			'L3MON4D3/LuaSnip',
+			{
+				'L3MON4D3/LuaSnip',
+				build = 'make install_jsregexp',
+			},
 			'saadparwaiz1/cmp_luasnip',
 			'onsails/lspkind.nvim',
 			{
