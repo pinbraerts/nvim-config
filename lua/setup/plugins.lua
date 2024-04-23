24d87618ddc0353689ac44022a40a984c153c4db
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-04-23 20:47:56 +0300

    resession configuration

--- lua/setup/plugins.lua
+++ lua/setup/plugins.lua
@@ -116,16 +116,16 @@ require 'lazy'.setup {
 			}
 			vim.api.nvim_create_autocmd("VimEnter", {
 				callback = function()
-					-- Only load the session if nvim was started with no args
 					if vim.fn.argc(-1) == 0 then
-						-- Save these to a different directory, so our manual sessions don't get polluted
 						resession.load(vim.fn.getcwd(), { dir = ".session", silence_errors = true })
 					end
 				end,
 			})
 			vim.api.nvim_create_autocmd("VimLeavePre", {
 				callback = function()
-					resession.save(vim.fn.getcwd(), { dir = ".session", notify = false })
+					if vim.fn.argc(-1) == 0 then
+						resession.save(vim.fn.getcwd(), { dir = ".session", notify = false })
+					end
 				end,
 			})
 		end,
