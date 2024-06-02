7092b3bfe93295c12110a44fa239f8773267badb
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-06-02 09:33:48 +0300

    treesitter: fix configuration + use filetypes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,70 @@
-local languages = { 'rust', 'c', 'cpp', 'python', 'lua', 'cuda', 'vimdoc', 'html', 'css', 'javascript', 'go' }
+local ft = require('filetypes')
 vim.wo.foldmethod = 'expr'
 vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 vim.o.foldlevel = 9999
+
+function config ()
+	require('nvim-treesitter.configs').setup {
+		ensure_installed = ft.treesitter,
+		auto_install = true,
+		sync_install = false,
+		ignore_install = {},
+		modules = {},
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false,
+		},
+		indent = { enable = true, disable = { 'c', 'cpp' } },
+		incremental_selection = { enable = true },
+		textobjects = {
+			select = {
+				enable = true,
+				lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+				keymaps = {
+					-- You can use the capture groups defined in textobjects.scm
+					['aa'] = '@parameter.outer',
+					['ia'] = '@parameter.inner',
+					['af'] = '@function.outer',
+					['if'] = '@function.inner',
+					['ac'] = '@class.outer',
+					['ic'] = '@class.inner',
+				},
+			},
+			move = {
+				enable = true,
+				set_jumps = true, -- whether to set jumps in the jumplist
+				goto_next_start = {
+					[']m'] = '@function.outer',
+					[']]'] = '@class.outer',
+				},
+				goto_next_end = {
+					[']M'] = '@function.outer',
+					[']['] = '@class.outer',
+				},
+				goto_previous_start = {
+					['[m'] = '@function.outer',
+					['[['] = '@class.outer',
+				},
+				goto_previous_end = {
+					['[M'] = '@function.outer',
+					['[]'] = '@class.outer',
+				},
+			},
+			swap = {
+				enable = true,
+				swap_next = {
+					['<leader>a'] = '@parameter.inner',
+				},
+				swap_previous = {
+					['<leader>A'] = '@parameter.inner',
+				},
+			},
+		},
+		context = {
+			enable = true,
+		},
+	}
+end
 
 return {
 	{
@@ -12,66 +75,7 @@ return {
 		},
 		build = ':TSUpdate',
 		lazy = false,
-		config = {
-			ensure_installed = languages,
-			auto_install = false,
-			sync_install = true,
-			ignore_install = {},
-			modules = {},
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = { enable = true, disable = { 'c', 'cpp' } },
-			incremental_selection = { enable = true },
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-					keymaps = {
-						-- You can use the capture groups defined in textobjects.scm
-						['aa'] = '@parameter.outer',
-						['ia'] = '@parameter.inner',
-						['af'] = '@function.outer',
-						['if'] = '@function.inner',
-						['ac'] = '@class.outer',
-						['ic'] = '@class.inner',
-					},
-				},
-				move = {
-					enable = true,
-					set_jumps = true, -- whether to set jumps in the jumplist
-					goto_next_start = {
-						[']m'] = '@function.outer',
-						[']]'] = '@class.outer',
-					},
-					goto_next_end = {
-						[']M'] = '@function.outer',
-						[']['] = '@class.outer',
-					},
-					goto_previous_start = {
-						['[m'] = '@function.outer',
-						['[['] = '@class.outer',
-					},
-					goto_previous_end = {
-						['[M'] = '@function.outer',
-						['[]'] = '@class.outer',
-					},
-				},
-				swap = {
-					enable = true,
-					swap_next = {
-						['<leader>a'] = '@parameter.inner',
-					},
-					swap_previous = {
-						['<leader>A'] = '@parameter.inner',
-					},
-				},
-			},
-			context = {
-				enable = true,
-			},
-		},
+		config = config,
 		keys = {
 			{ '<leader>ti', '<cmd>InspectTree<cr>', desc = '[T]ree [i]nspect', silent = true, },
 		},
@@ -82,14 +86,7 @@ return {
 		dependencies = {
 			'nvim-treesitter/nvim-treesitter',
 		},
-		ft = {
-			'html', 'xml',
-			-- 'javascript', 'typescript', 'javascriptreact',
-			'typescriptreact', 'svelte', 'vue', 'tsx', 'jsx', 'rescript',
-			'php',
-			'markdown',
-			'astro', 'glimmer', 'handlebars', 'hbs'
-		},
+		ft = ft.webdev,
 	},
 
 }
