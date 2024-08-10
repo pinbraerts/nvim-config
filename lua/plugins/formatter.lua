4b4412b85ba134ce49f9489d0fec71c538d22ea1
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-08-10 10:06:07 +0300

    format: add disable functionality

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -3,21 +3,46 @@ return {
   {
     "stevearc/conform.nvim",
     dependencies = "williamboman/mason.nvim",
-    opts = {
-      formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "black" },
-        c = { "clangformat" },
-        cpp = { "clangformat" },
-        rust = { "rustfmt", lsp_format = "fallback" },
-        fennel = { "fnlfmt" },
-        javascript = { "prettierd", "prettier", stop_after_first = true },
-      },
-      format_on_save = {
-        timeout = 500,
-        lsp_format = "fallback",
-      },
-    },
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          lua = { "stylua" },
+          python = { "black" },
+          c = { "clangformat" },
+          cpp = { "clangformat" },
+          rust = { "rustfmt", lsp_format = "fallback" },
+          fennel = { "fnlfmt" },
+          javascript = { "prettierd", "prettier", stop_after_first = true },
+        },
+        format_on_save = function(bufnr)
+          -- Disable with a global or buffer-local variable
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+          end
+          return { timeout_ms = 500, lsp_format = "fallback" }
+        end,
+      })
+
+      vim.api.nvim_create_user_command("FormatDisable", function(args)
+        if args.bang then
+          vim.g.disable_autoformat = true
+        else
+          vim.b.disable_autoformat = true
+        end
+      end, {
+        desc = "Disable autoformat-on-save",
+        bang = true,
+      })
+      vim.api.nvim_create_user_command("FormatEnable", function(args)
+        vim.b.disable_autoformat = false
+        if args.bang then
+          vim.g.disable_autoformat = false
+        end
+      end, {
+        desc = "Re-enable autoformat-on-save",
+        bang = true,
+      })
+    end,
   },
 
   {
