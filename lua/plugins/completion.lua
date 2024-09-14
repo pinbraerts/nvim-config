db4543e11047972675d0bf8862245feb6afb3ba8
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-09-14 22:25:55 +0300

    completion: try clangd score

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -40,6 +40,7 @@ local function setup()
         cmp.config.compare.offset,
         cmp.config.compare.exact,
         cmp.config.compare.recently_used,
+        require("clangd_extensions.cmp_scores"),
         cmp.config.compare.kind,
         cmp.config.compare.sort_text,
         cmp.config.compare.length,
