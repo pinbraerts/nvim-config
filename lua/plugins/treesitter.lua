ef8dc183e70b47cd092faeb7aed8477a57561749
Dmitry Shirshov <shirshov.da@phystech.edu>
2025-01-05 23:21:16 +0300

    feat(treesitter): modern config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,68 +2,6 @@ vim.wo.foldmethod = "expr"
 vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.o.foldlevel = 9999
 
-function config()
-  require("nvim-treesitter.configs").setup({
-    auto_install = true,
-    sync_install = false,
-    ignore_install = {},
-    modules = {},
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    indent = { enable = true, disable = { "c", "cpp" } },
-    incremental_selection = { enable = true },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-        keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ["aa"] = "@parameter.outer",
-          ["ia"] = "@parameter.inner",
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
-        },
-      },
-      move = {
-        enable = true,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        goto_next_start = {
-          ["]m"] = "@function.outer",
-          ["]]"] = "@class.outer",
-        },
-        goto_next_end = {
-          ["]M"] = "@function.outer",
-          ["]["] = "@class.outer",
-        },
-        goto_previous_start = {
-          ["[m"] = "@function.outer",
-          ["[["] = "@class.outer",
-        },
-        goto_previous_end = {
-          ["[M"] = "@function.outer",
-          ["[]"] = "@class.outer",
-        },
-      },
-      swap = {
-        enable = true,
-        swap_next = {
-          ["<leader>a"] = "@parameter.inner",
-        },
-        swap_previous = {
-          ["<leader>A"] = "@parameter.inner",
-        },
-      },
-    },
-    context = {
-      enable = true,
-    },
-  })
-end
-
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -73,7 +11,80 @@ return {
     },
     build = ":TSUpdate",
     lazy = false,
-    config = config,
+    opts = {
+      ensure_installed = {},
+      auto_install = true,
+      sync_install = false,
+      ignore_install = {},
+      modules = {},
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = { enable = true, disable = { "c", "cpp" } },
+      incremental_selection = { enable = true },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          include_surrounding_whitespace = function(options)
+            return options.selection_mode == "V"
+          end,
+          keymaps = {
+            ["a="] = { query = "@assignment.outer", desc = "[a]round of assignment" },
+            ["="] = { query = "@assignment.lhs", desc = "[i]nside of assignment" },
+            ["i="] = { query = "@assignment.rhs", desc = "[i]nside of assignment" },
+            ["a,"] = { query = "@parameter.outer", desc = "[a]round argument" },
+            ["i,"] = { query = "@parameter.inner", desc = "[i]nside argument" },
+            ["ad"] = { query = "@function.outer", desc = "[a]round function [d]efinition" },
+            ["id"] = { query = "@function.inner", desc = "[i]nside function [d]efinition" },
+            ["af"] = { query = "@call.outer", desc = "[a]round [f]unction call" },
+            ["if"] = { query = "@call.inner", desc = "[i]nside [f]unction call" },
+            ["ac"] = { query = "@class.outer", desc = "[a]round [c]lass" },
+            ["ic"] = { query = "@class.inner", desc = "[i]nside [c]lass" },
+          },
+          selection_modes = {
+            ["@function.outer"] = "V",
+            ["@class.outer"] = "V",
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true,
+          goto_next_start = {
+            ["]m"] = { query = "@function.outer", desc = "next [m]ethod start" },
+            ["]]"] = { query = "@class.outer", desc = "next class start" },
+          },
+          goto_next_end = {
+            ["]M"] = { query = "@function.outer", desc = "next [m]ethod end" },
+            ["]["] = { query = "@class.outer", desc = "next class end" },
+          },
+          goto_previous_start = {
+            ["[m"] = { query = "@function.outer", desc = "previous [m]ethod start" },
+            ["[["] = { query = "@class.outer", desc = "previous class start" },
+          },
+          goto_previous_end = {
+            ["[M"] = { query = "@function.outer", desc = "previous [m]ethod end" },
+            ["[]"] = { query = "@class.outer", desc = "previous class end" },
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>."] = { query = "@parameter.inner", desc = "swap with next parameter" },
+          },
+          swap_previous = {
+            ["<leader>,"] = { query = "@parameter.inner", desc = "swap with previous parameter" },
+          },
+        },
+      },
+      context = {
+        enable = true,
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
     keys = {
       { "<leader>ti", "<cmd>InspectTree<cr>", desc = "[T]ree [i]nspect", silent = true },
     },
@@ -84,5 +95,12 @@ return {
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
     },
+  },
+
+  {
+    "andymass/vim-matchup",
+    init = function()
+      vim.g.matchup_matchparen_offscreen = { method = "popup" }
+    end,
   },
 }
