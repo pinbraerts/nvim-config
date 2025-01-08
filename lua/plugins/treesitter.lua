698c7edc8768d05aec54d6a8b98060c882c73b0d
Dmitry Shirshov <shirshov.da@phystech.edu>
2025-01-08 15:04:05 +0300

    fix(matchup): do not highlight the match

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -100,6 +100,7 @@ return {
   {
     "andymass/vim-matchup",
     init = function()
+      vim.g.matchup_matchparen_enabled = 0
       vim.g.matchup_matchparen_offscreen = { method = "popup" }
     end,
   },
