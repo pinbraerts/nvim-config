d0acb3b77da4d849cb4b89eb5dbca0d631df2f7c
Dmitry Shirshov <shirshov.da@phystech.edu>
2025-02-01 14:32:05 +0300

    refactor(integration): move to separate file

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -68,40 +68,6 @@ return {
   },
 
   {
-    "nanotee/zoxide.vim",
-    cond = function()
-      return vim.fn.executable("zoxide") ~= 0
-    end,
-    cmd = {
-      "Z",
-      "Lz",
-      "Tz",
-      "Zi",
-      "Lzi",
-      "Tzi",
-    },
-  },
-
-  {
-    "mikavilpas/yazi.nvim",
-    cond = function()
-      return vim.fn.executable("yazi") ~= 0
-    end,
-    cmd = { "Yazi" },
-    keys = {
-      {
-        "<leader>fb",
-        "<cmd>Yazi cwd<cr>",
-        desc = "Open yazi file manager in current working directory",
-      },
-    },
-    opts = {
-      open_for_directories = false,
-      use_yazi_client_id_flag = true,
-    },
-  },
-
-  {
     "folke/trouble.nvim",
     cmd = { "Trouble" },
     config = true,
@@ -191,29 +157,5 @@ return {
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     keys = { { "gc", mode = { "n", "v" } } },
     config = true,
-  },
-
-  {
-    "aserowy/tmux.nvim",
-    enable = vim.fn.executable("tmux") ~= 0,
-    cond = function()
-      return os.getenv("TMUX") ~= nil
-    end,
-    config = function()
-      local tmux = require("tmux")
-      tmux.setup({
-        navigation = {
-          enable_default_keybindings = true,
-          cycle_navigation = false,
-          persist_zoom = true,
-        },
-        copy_sync = {
-          sync_clipboard = true,
-        },
-        resize = {
-          enable_default_keybindings = false,
-        },
-      })
-    end,
   },
 }
