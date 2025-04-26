f6ef783fe48956316081b62a4382722ecd3235cd
Dmitry Shirshov <shirshov.da@phystech.edu>
2025-04-26 21:28:29 +0300

    feat(hover): use my highlights provider

--- lua/plugins/hover.lua
+++ lua/plugins/hover.lua
@@ -10,6 +10,7 @@ return {
         require("hover.providers.gh_user")
         -- require('hover.providers.jira')
         require("hover.providers.man")
+        require("hover.providers.highlight")
         -- require('hover.providers.dictionary')
       end,
       preview_opts = {
@@ -29,20 +30,9 @@ return {
 
     vim.keymap.set("n", "K", hover.hover, { desc = "hover.nvim" })
     vim.keymap.set("n", "gK", hover.hover_select, { desc = "hover.nvim (select)" })
-    vim.keymap.set("n", "<C-p>", function()
-      hover.hover_switch("previous")
-    end, { desc = "hover.nvim (previous source)" })
-    vim.keymap.set("n", "<C-n>", function()
-      hover.hover_switch("next")
-    end, { desc = "hover.nvim (next source)" })
-
-    -- vim.keymap.set('n', '<MouseMove>', require('hover').hover_mouse, { desc = "hover.nvim (mouse)" })
-    -- vim.o.mousemoveevent = true
   end,
   keys = {
     { "K", desc = "hover.nvim" },
     { "gK", desc = "hover.nvim (select)" },
-    { "<C-p>", desc = "hover.nvim (previous source)" },
-    { "<C-n>", desc = "hover.nvim (next source)" },
   },
 }
